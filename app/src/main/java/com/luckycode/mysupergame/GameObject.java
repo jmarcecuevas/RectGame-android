2711d56a0fe3bfde6a1af696713d07415b0152b0
marceCuevas16 <marce.cuevas16@gmail.com>
2018-03-08 00:35:05 -0300

    Initial commit

---
+++ app/src/main/java/com/luckycode/mysupergame/GameObject.java
@@ -0,0 +1,12 @@
+package com.luckycode.mysupergame;
+
+import android.graphics.Canvas;
+
+/**
+ * Created by marcelocuevas on 3/8/18.
+ */
+
+public interface GameObject {
+    void draw(Canvas canvas);
+    void update();
+}
---
+++ app/src/main/java/com/luckycode/mysupergame/RectPlayer.java
@@ -0,0 +1,37 @@
+package com.luckycode.mysupergame;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+
+/**
+ * Created by marcelocuevas on 3/8/18.
+ */
+
+public class RectPlayer implements GameObject {
+    private Rect rect;
+    private int color;
+
+    public RectPlayer(Rect rect,int color){
+        this.rect = rect;
+        this.color = color;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setColor(color);
+        canvas.drawRect(rect,paint);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    public void update(Point point){
+        rect.set(point.x - rect.width()/2 , point.y - rect.height()/2 , point.x + rect.width()/2,
+                point.y + rect.height()/2);
+    }
+}
